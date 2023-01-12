c079364695594c1375bda8087e795fd1a852b9b3
chandni-tilasi <96279259+chandni-tilasi@users.noreply.github.com>
2023-01-12 21:02:01 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,27 @@
-import React from 'react'
-import '../styles/App.css';
-
-// You are given an initial state {name: "John Doe", enrollmentNo: "12345678", age: 34}. Complete the given class based react component 'App' such that it returns 'Hello, my name is name' in <h1> tag and 'I am age years old and my enrollment no is enrollmentNo' in <p>. Also insert a button which increases the age by 1 when clicked on it. boilerplate is showing error because the code is missing. Complete the code
-
-const obj = {name: "John Doe", enrollmentNo: "12345678", age: 34}
-
-
-function increment(){
-    let countAge = obj.age;
-    obj.age = countAge++;
-}
+import React from "react";
+import "../styles/App.css";
 
 class App extends React.Component {
-  
-    render(){
-        return <div>
+  constructor(props) {
+    super(props);
+    this.state = { name: "John Doe", enrollmentNo: "12345678", age: 34 };
+  }
+  incrementNumber = () => {
+    this.setState({ age: this.state.age + 1 });
+  };
 
-            <h1> Hello, my name is {obj.name} </h1>
-            <p> I am {obj.age} years old and my enrollment no is {obj.enrollmentNo}  </p>
-            <button onClick={increment}>
-            increase age
-</button>
-        </div>
-    }
-    
-    
+  render() {
+    return (
+      <div>
+        <h1>Hello, my name is {this.state.name}</h1>
+        <p>
+          I am {this.state.age} years old and my enrollment no is{" "}
+          {this.state.enrollmentNo}
+        </p>
+        <button onClick={this.incrementNumber}>Increment</button>
+      </div>
+    );
+  }
 }
-
 
 export default App;
